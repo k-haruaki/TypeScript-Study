ea3608ee26a46de618fb4e8671f5183fc60377cf
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2022-11-18 16:44:43 +0900

    TypeScript勉強のための環境構築

---
+++ dist/server.js
@@ -0,0 +1,11 @@
+// 足し算を行うプログラム
+const a = 5;
+const b = 10;
+const result = sum(a, b);
+// 結果表示
+console.log(`${a} + ${b} = ${result}です`);
+// 関数 sumを定義
+function sum(a, b) {
+    return a + b;
+}
+//# sourceMappingURL=server.js.map
