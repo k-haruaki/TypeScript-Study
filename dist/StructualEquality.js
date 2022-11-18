3f17c2eab07b29384f557e6d311ded9d82f79c3b
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2022-11-18 17:37:14 +0900

    run start コマンドで引数を設定できるようにアップデート

---
+++ dist/StructualEquality.js
@@ -0,0 +1,13 @@
+const list = [
+    {
+        id: 'foo',
+        display: 'Foo Select'
+    },
+    {
+        id: 'bar',
+        display: 'Bar Select'
+    },
+];
+const fooIndex = list.map(i => i.id).indexOf('foo');
+console.log(fooIndex);
+//# sourceMappingURL=StructualEquality.js.map
