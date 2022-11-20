4bf8f4fec5d3428d142006c02dc9b55ea946c9a8
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2022-11-20 23:41:12 +0900

    継承の使い方について

--- dist/Class.js
+++ dist/Class.js
@@ -8,9 +8,21 @@ class Point {
     }
 }
 var p1 = new Point(5, 10);
-console.log(p1);
 var p2 = new Point(10, 20);
-console.log(p2);
 var p3 = p1.add(p2);
 console.log(p3);
+class Point3D extends Point {
+    constructor(x, y, z) {
+        super(x, y);
+        this.z = z;
+    }
+    add(point) {
+        var point2D = super.add(point);
+        return new Point3D(point2D.x, point2D.y, this.z + point.z);
+    }
+}
+var p4 = new Point3D(10, 20, 30);
+var p5 = new Point3D(40, 50, 60);
+var p6 = p4.add(p5);
+console.log(p6);
 //# sourceMappingURL=Class.js.map
--- src/Class.ts
+++ src/Class.ts
@@ -11,10 +11,24 @@ class Point {
 }
 
 var p1 = new Point(5, 10);
-console.log(p1);
-
 var p2 = new Point(10, 20);
-console.log(p2)
-
 var p3 = p1.add(p2);
 console.log(p3)
+
+
+class Point3D extends Point {
+    z: number;
+    constructor(x: number, y: number, z: number) {
+        super(x, y);
+        this.z = z;
+    }
+    add(point: Point3D) {
+        var point2D = super.add(point);
+        return new Point3D(point2D.x, point2D.y, this.z + point.z);
+    }
+}
+
+var p4 = new Point3D(10, 20, 30);
+var p5 = new Point3D(40, 50, 60);
+var p6 = p4.add(p5);
+console.log(p6);
