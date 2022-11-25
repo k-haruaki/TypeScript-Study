8712158e5d73de491a577aa677e07a19fdf25169
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2022-11-25 17:05:16 +0900

    アロー関数を作成してみる

---
+++ src/ArrowFunctions.js
@@ -0,0 +1,26 @@
+// 通常の関数を使ったパターン
+function Person(age) {
+    this.age = age;
+    this.growOld = function() {
+        this.age++;
+    }
+}
+
+var person = Person(1);
+setTimeout(person.growOld, 1000);
+
+setTimeout(function() {
+    console.log(person.age);
+}, 2000);
+
+// アロー関数で置き換えてみる
+function Person(age) {
+    this.age = age;
+    this.growOld = () => {
+        this.age++;
+    }
+}
+var person = new Person(1);
+setTimeout(person.growOld,1000);
+
+setTimeout(function() { console.log(person.age); },2000); // 2
