00b6fcff6fd3278dc248be8303290971dff9fd48
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2022-12-06 23:47:36 +0900

    chenge name

--- src/ArrowFunctions.js
+++ src/ArrowFunctions.js
@@ -24,3 +24,5 @@ var person = new Person(1);
 setTimeout(person.growOld,1000);
 
 setTimeout(function() { console.log(person.age); },2000); // 2
+
+setTimeout(function() { console.log(person.age); },2000); // 2
