a5257fe62bcf9fcc30299fbe94a65ec3715cbcdf
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-02-18 23:36:44 +0900

    eslint

--- src/Class.ts
+++ src/Class.ts
@@ -1,33 +1,32 @@
 // クラスについて
 class Point {
-    x: number;
-    y: number;
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-    }
-    add(point: Point) {
-        return new Point(this.x + point.x, this.y + point.y);
-    }
+  x: number;
+  y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+  add(point: Point) {
+    return new Point(this.x + point.x, this.y + point.y);
+  }
 }
 
 var p1 = new Point(5, 10);
 var p2 = new Point(10, 20);
 var p3 = p1.add(p2);
-console.log(p3)
-
+console.log(p3);
 
 // 軽症について
 class Point3D extends Point {
-    z: number;
-    constructor(x: number, y: number, z: number) {
-        super(x, y);
-        this.z = z;
-    }
-    add(point: Point3D) {
-        var point2D = super.add(point);
-        return new Point3D(point2D.x, point2D.y, this.z + point.z);
-    }
+  z: number;
+  constructor(x: number, y: number, z: number) {
+    super(x, y);
+    this.z = z;
+  }
+  add(point: Point3D) {
+    var point2D = super.add(point);
+    return new Point3D(point2D.x, point2D.y, this.z + point.z);
+  }
 }
 
 var p4 = new Point3D(10, 20, 30);
@@ -35,13 +34,12 @@ var p5 = new Point3D(40, 50, 60);
 var p6 = p4.add(p5);
 console.log(p6);
 
-
 // 静的メンバについて
 class Something {
-    static instances = 0;
-    constructor() {
-        Something.instances++;
-    }
+  static instances = 0;
+  constructor() {
+    Something.instances++;
+  }
 }
 
 var s1 = new Something();
@@ -49,35 +47,34 @@ var s2 = new Something();
 var s3 = new Something();
 console.log(Something.instances);
 
-
 // アクセス修飾子
 class FooBase {
-    public x: number;
-    protected y : number;
-    private z : number;
+  public x: number;
+  protected y: number;
+  private z: number;
 }
 
 // インスタンスにおける効果
 var foo = new FooBase();
 foo.x;
 // protectedのためエラーになる
-// foo.y;  
+// foo.y;
 
 // privateのためエラーになる
 // foo.z;
 
 class FooChild extends FooBase {
-    constructor() {
-        super();
-        this.x;
-        this.y;
-        // privateのためエラーになる
-        // this.z;
-    }
+  constructor() {
+    super();
+    this.x;
+    this.y;
+    // privateのためエラーになる
+    // this.z;
+  }
 }
 
 abstract class FooCommand {
-//   abstract execute(): string;
+  //   abstract execute(): string;
 }
 
 class BarErrorCommand extends FooCommand {} // 'BarErrorCommand'は抽象メンバー'execute'を実装する必要があります
@@ -92,21 +89,19 @@ const barCommand = new BarCommand();
 
 barCommand.execute(); // コマンドBarが実行されました
 
-
 // コンストラクタの定義
 class Foo {
-    x: number;
-    constructor(x: number) {
-        this.x = x;
-    }
+  x: number;
+  constructor(x: number) {
+    this.x = x;
+  }
 }
-
 
 // プロパティ初期化子
 // 例文のmembersで型を指定しないとエラーになるため、型の指定が必要
 class Bar {
-    members: string[] = [];  // コンストラクタなしで直接メンバ変数を初期化できます
-    add(x: string) {
-        this.members.push(x);
-    }
+  members: string[] = []; // コンストラクタなしで直接メンバ変数を初期化できます
+  add(x: string) {
+    this.members.push(x);
+  }
 }
