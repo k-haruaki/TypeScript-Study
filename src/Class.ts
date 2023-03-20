9222fafc8c976fc95ea58f8e54402320d277e2fc
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-03-20 18:51:26 +0900

    コメントの修正

--- src/Class.ts
+++ src/Class.ts
@@ -16,7 +16,7 @@ var p2 = new Point(10, 20);
 var p3 = p1.add(p2);
 console.log(p3);
 
-// 軽症について
+// 継承について
 class Point3D extends Point {
   z: number;
   constructor(x: number, y: number, z: number) {
