43cf6095f483bab491fc3e931e686c8313a25b8b
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-04-21 23:44:09 +0900

    typescriptの本に書いてあったasを使った方法の説明

--- src/Class.ts
+++ src/Class.ts
@@ -148,3 +148,12 @@ const myName3: IName = {
   lastName: "haruaki",
   age: 29, // 定義は違うが、INameはinterfaceで再定義しているので、ageが必要になる
 };
+
+const Sample = (value: string | number) => {
+  // valueは、string | numberであるため、sampleValueには代入できない
+  // const sampleValue: string = value;
+
+  // valueをasでstringに指定できるが、型の安全性が崩れてしまうため、よくない
+  const sampleValue: string = value as string;
+  console.log(sampleValue);
+};
