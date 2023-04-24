28426912bf5920848d69cdd31c445e4e3ba11b7e
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-04-24 21:40:25 +0900

    マイナスの処理を追加

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,21 @@
-// 足し算を行うプログラム
-const a = 5;
-const b = 10;
+// 計算プログラム
+const a: number = 5;
+const b: number = 10;
 
-const result = sum(a, b);
+// 関数 足し算を定義
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
 
 // 結果表示
+const result = sum(a, b);
 console.log(`${a} + ${b} = ${result}です`);
 
-// 関数 sumを定義
-function sum(a: number, b: number): number{
-  return a + b;
-}
+// 関数 引き算を定義
+const minus = (a: number, b: number): number => {
+  return a - b;
+};
+
+// 結果表示
+const minusResult = minus(a, b);
+console.log(`${a} - ${b} = ${minusResult}です`);
