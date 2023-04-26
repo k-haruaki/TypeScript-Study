bf4bdbdc002c97e0abc805238a610f0348f3cbed
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-04-26 11:51:49 +0900

    Promiseのジェネリック型の説明を記載

---
+++ src/Promis/PromiseGeneric.ts
@@ -0,0 +1,19 @@
+type User = {
+  name: string;
+  age: number;
+};
+
+function getUser(): Promise<User> {
+  return new Promise((resolve) => {
+    const user: User = {
+      name: "太郎",
+      age: 10,
+    };
+    resolve(user);
+  });
+}
+
+getUser().then((user: User) => {
+  console.log(user);
+  // @log: { "name": "太郎", "age": 10 }
+});
