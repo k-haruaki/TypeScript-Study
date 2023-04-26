94f90c71146ab0a80a43c5f6d904698fa8bc1182
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-04-26 11:59:47 +0900

    Promiseのrejectedの説明を記載

--- src/Promis/PromiseGeneric.ts
+++ src/Promis/PromiseGeneric.ts
@@ -17,3 +17,38 @@ getUser().then((user: User) => {
   console.log(user);
   // @log: { "name": "太郎", "age": 10 }
 });
+
+// 例：thenのたびに新しく定数を定義する
+const promise1: Promise<number> = Promise.resolve(1);
+const promise2: Promise<string> = promise1.then((value) => `${value}`);
+
+// 例：thenでメソッドチェーン
+const promise: Promise<boolean> = Promise.resolve("1")
+  .then((value) => Number(value))
+  .then((value) => value > 0);
+
+// コールバック内で例外を投げるとそのPromiseは拒否される
+Promise.resolve(1)
+  .then(() => {
+    throw new Error();
+  })
+  .then(() => {
+    console.log("fulilled");
+  })
+  .catch(() => {
+    console.log("rejected");
+  });
+// @log: rejected
+
+// コールバック内で拒否されたPromiseを返しても、そのPromiseは拒否される
+Promise.resolve(1)
+  .then(() => {
+    return Promise.reject(new Error());
+  })
+  .then(() => {
+    console.log("fulilled");
+  })
+  .then(() => {
+    console.log("rejected");
+  });
+// @log: rejected
