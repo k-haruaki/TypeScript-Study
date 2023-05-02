4372ee14cee70e7b724a9637716745ef3aea2085
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-05-02 21:41:56 +0900

    console.logの追加

--- src/Set/Set.tsx
+++ src/Set/Set.tsx
@@ -1,3 +1,6 @@
 const fruits = new Set(["apple", "orange", "banana"]);
 console.log(fruits);
 // Set { 'apple', 'orange', 'banana' }
+
+const fruits2 = new Set(["apple", "orange", "banana"]);
+console.log(fruits2);
