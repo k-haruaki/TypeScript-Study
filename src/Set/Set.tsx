4cc63b3f34cfa45539fa77cd54af45b4bf24b04c
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-05-03 10:27:17 +0900

    remove set2

--- src/Set/Set.tsx
+++ src/Set/Set.tsx
@@ -1,6 +1,3 @@
 const fruits = new Set(["apple", "orange", "banana"]);
 console.log(fruits);
 // Set { 'apple', 'orange', 'banana' }
-
-const fruits2 = new Set(["apple", "orange", "banana"]);
-console.log(fruits2);
