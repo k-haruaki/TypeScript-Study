361a5ff1e450683724fa51c238ec54ab2329dd8c
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-05-07 23:32:02 +0900

    三項演算子の説明を追加

--- src/ReactTypeScriptCheatsheet/ConditionComponent.tsx
+++ src/ReactTypeScriptCheatsheet/ConditionComponent.tsx
@@ -1,3 +1,8 @@
 const ConditionComponent = ({ flg }: { flg: boolean }) => {
   return <p>{flg ? "trueの時のJSX" : "falseの時のJSX"}</p>;
 };
+
+// finish = true で finishクラスを追加
+const TaskItem = (props: { task: string; finish: boolean }) => {
+  return <li className={props.finish ? "finish" : ""}>{props.task}</li>;
+};
