85c0e2ce19f61c8b0579f05c057de6d1b3e57b01
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-06-03 18:46:33 +0900

    objectで必要な値を検索する方法を追加

---
+++ src/SearchSample/Object.ts
@@ -0,0 +1,14 @@
+// 連想配列（Object）のサンプル
+const sampleObject = {
+  ibaraki: 2840403,
+  kanagawa: 9221129,
+  gunma: 1915035,
+  saitama: 7331256,
+  chiba: 6267579,
+  tokyo: 13995469,
+  tochigi: 1910502,
+};
+
+// tokyo に対応する値を取得する
+const objectResult = sampleObject.tokyo;
+console.log(objectResult); // output: 13995469
