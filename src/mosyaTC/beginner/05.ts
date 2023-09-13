85cc0925ff31082ef6604fd6de563132e39d917d
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-09-14 00:15:27 +0900

    add 05.ts

---
+++ src/mosyaTC/beginner/05.ts
@@ -0,0 +1,45 @@
+// ========== typescriptの本 3.4.3 ==========
+type HasName = {
+    name: string;
+}
+type Family<Parent extends HasName, Child extends HasName> = {
+    mother: Parent;
+    father: Parent;
+    child: Child;
+}
+
+// 下記の定義では、HasNameにnumberは含まれていないのでエラーになる
+// type T = Family<number, string>
+
+type Animal = {
+    name: string;
+}
+
+type Human = {
+    name: string;
+    age: number;
+}
+
+type T = Family<Animal, Human>
+
+type Family02<Parent extends HasName, Child extends Parent> = {
+    mother: Parent;
+    father: Parent;
+    child: Child;
+}
+
+type S = Family02<Animal, Human>
+// エラーになる
+// AnimalはHumanの部分型ではないため
+// type TT = Family02<Human, Animal>
+
+
+
+// ========== 問題 ==========
+// Push型を定義してください
+// 例えば、Push<[1, 2], '3'>は[1, 2, '3']を返します
+
+type Result = Push<[1, 2], '3'> // [1, 2, '3']
+
+// ========== 回答 ==========
+type Push<T extends any[], U> = [...T, U]
